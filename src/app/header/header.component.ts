c36f27ca1fae2b577c91ba692d9c4cde6a1d49bb
Swapnali <bswapnali@gmail.com>
2023-05-20 14:59:11 +0530

    -swapnali

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-header',
@@ -19,6 +20,16 @@ export class HeaderComponent implements OnInit {
     const dialogRef = this.dialog.open(LoginComponent, {
       width: '595px',
       height: '450px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+  onSign(): void {
+    const dialogRef = this.dialog.open(SignupComponent, {
+      width: '1300px',
+      height: '550px',
     });
 
     dialogRef.afterClosed().subscribe(result => {
