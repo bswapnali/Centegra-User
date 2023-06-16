35828bee4dca4dc9a3a0a39a33db6106f7faa862
SantoshQantiRoy <santosh.roy@quantique.ai>
2023-06-16 15:53:34 +0530

    Santosh160623

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,41 +1,47 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';  
+import { HttpClient } from '@angular/common/http';
 import { LoginService } from '../service/login.service';
 import { SharedserviceService } from '../service/sharedservice.service';
+import { SignupComponent } from '../signup/signup.component';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
- 
-  show: boolean= false;
+  show: boolean = false;
   toggle = true;
   private fb = inject(FormBuilder);
-  email:any;
-  password:any;
-  constructor(private route:Router, public dialog:MatDialog, private loginService: LoginService,public sharedservice:SharedserviceService){}
+  email: any;
+  password: any;
+  constructor(
+    private route: Router,
+    public dialog: MatDialog,
+    private loginService: LoginService,
+    public sharedservice: SharedserviceService
+  ) {}
   loginForm = this.fb.group({
     // company: null,
     email: [null, [Validators.required, this.sharedservice.emailValidator()]],
     password: [null, Validators.required],
-
-
-
-
   });
-  login(){
-    this.route.navigate(['/login'])
+  login() {
+    this.route.navigate(['/login']);
   }
-  
-  clear(){
-  // this.email ="";
-  // this.password = "";
-  this.show = true;
+
+  clear() {
+    // this.email ="";
+    // this.password = "";
+    this.show = true;
   }
 
   // getErrorMessage() {
@@ -49,29 +55,32 @@ export class LoginComponent {
   //     if (this.password.hasError('required')) {
   //       return 'Enter a valid password';
   //     }
-  
+
   //     return this.email.hasError('email') ? 'Not a valid password' : '';
   //   }
-  
-  
+
   onSubmit(): void {
-    this.loginService.login(this.email, this.password)
-      .subscribe(
-        (response) => {
-          // Handle successful login
-          console.log('logined'+this.email);
-        },
-        (error) => {
-          // Handle login error
-          console.log('error');
-          console.log('logined'+this.email);
-        }
-      );
+    this.loginService.login(this.email, this.password).subscribe(
+      (response) => {
+        // Handle successful login
+        console.log('logined' + this.email);
+      },
+      (error) => {
+        // Handle login error
+        console.log('error');
+        console.log('logined' + this.email);
+      }
+    );
     console.log('Thanks!');
   }
-  
+  onSign(): void {
+    const dialogRef = this.dialog.open(SignupComponent, {
+      width: '1300px',
+      height: '550px',
+    });
 
-  
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
 }
-
-  
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,26 +5,30 @@ import { AccountCreatedDialogComponent } from './account-created-dialog/account-
 import { MatDialog } from '@angular/material/dialog';
 import { SharedserviceService } from '../service/sharedservice.service';
 import { SignupService } from '../service/signup.service';
+import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
-     
   toggle = true;
   private fb = inject(FormBuilder);
-    firstName: any;
-    email: any;
-    lastname: any; 
-    compname: any;
-    desig: any;
-    pswd: any;
-    cpswd: any; 
-    mobno: any;
-  constructor(private route:Router, public dialog:MatDialog,
-    public sharedservice:SharedserviceService ,  private signupService: SignupService, ){}
-    signupForm = this.fb.group({
+  firstName: any;
+  email: any;
+  lastname: any;
+  compname: any;
+  desig: any;
+  pswd: any;
+  cpswd: any;
+  mobno: any;
+  constructor(
+    private route: Router,
+    public dialog: MatDialog,
+    public sharedservice: SharedserviceService,
+    private signupService: SignupService
+  ) {}
+  signupForm = this.fb.group({
     // company: null,
     firstName: [null, Validators.required],
     email: [null, [Validators.required, this.sharedservice.emailValidator()]],
@@ -33,8 +37,7 @@ export class SignupComponent {
     desig: [null, Validators.required],
     pswd: [null, Validators.required],
     cpswd: [null, Validators.required],
-    mobno: [null, Validators.required]
-    
+    mobno: [null, Validators.required],
   });
 
   hasUnitNumber = false;
@@ -53,43 +56,53 @@ export class SignupComponent {
   //   {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
   //   {name: 'Florida', abbreviation: 'FL'},
   // ];
+  onLogin(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '50%',
+      height: '80%',
+    });
 
-  
-  signup(){
-    this.route.navigate(['/login'])
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  signup() {
+    this.route.navigate(['/login']);
   }
   onSubmit(): void {
-    this.signupService.signup(this.firstName,this.email,
-      this.lastname,this.compname,
-      this.desig, this.pswd,this.cpswd,this.mobno 
+    this.signupService
+      .signup(
+        this.firstName,
+        this.email,
+        this.lastname,
+        this.compname,
+        this.desig,
+        this.pswd,
+        this.cpswd,
+        this.mobno
       )
-     
-    .subscribe(
-      (response) => {
-        // Handle successful login
-        console.log('signed'+this.email);
-      },
-      (error) => {
-        // Handle login error
-        console.log('error');
-        console.log('signed'+this.email);
-      }
-    );
-  console.log('Thanks!');
-}
 
-
-
-    // const dialogRef = this.dialog.open(AccountCreatedDialogComponent, {
-    //   width: '1100px',
-    //   height: '500px',
-    // });
-
-    // dialogRef.afterClosed().subscribe(res => {
-    //   console.log('The dialog was closed');
-    // });
-  
-  
+      .subscribe(
+        (response) => {
+          // Handle successful login
+          console.log('signed' + this.email);
+        },
+        (error) => {
+          // Handle login error
+          console.log('error');
+          console.log('signed' + this.email);
+        }
+      );
+    console.log('Thanks!');
   }
 
+  // const dialogRef = this.dialog.open(AccountCreatedDialogComponent, {
+  //   width: '1100px',
+  //   height: '500px',
+  // });
 
+  // dialogRef.afterClosed().subscribe(res => {
+  //   console.log('The dialog was closed');
+  // });
+}
