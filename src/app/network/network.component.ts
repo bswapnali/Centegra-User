540e3fa0c2dedd8517ec81bc48b651abf0b994a4
Pinky Gupta <pinky@quantique.ai>
2023-06-20 15:13:44 +0530

    is-pinky

--- src/app/network/network.component.ts
+++ src/app/network/network.component.ts
@@ -29,11 +29,16 @@ SelectLocation(): void {
       console.log('The dialog was closed');
     });
   }
-  ratings = [{number:1 ,rate:'1'},
-    {number:2 ,rate:'2.5'}, 
-    {number:3 ,rate:'3.5'}, 
-    {number:4.5 ,rate:'4.5'}, 
-    {number:5.5 ,rate:'5.5'}];
+  ratings = [
+    { name: 'Item 1', rating: 4.5 },
+    { name: 'Item 2', rating: 3.5 },
+    { name: 'Item 3', rating: 2 },
+  ];
+  // ratings = [{number:1 ,rate:'1'},
+  //   {number:2 ,rate:'2.5'}, 
+  //   {number:3 ,rate:'3.5'}, 
+  //   {number:4.5 ,rate:'4.5'}, 
+  //   {number:5.5 ,rate:'5.5'}];
  
   
   // onRadioButtonChange(event) { 
@@ -50,29 +55,41 @@ SelectLocation(): void {
   //   return rating % 1 === 0 ? rating.toString() : rating.toFixed(1);
   // }
 
-  getStarRating(rating: number): string {
-    let starRating = '';
-    for (let i = 1; i <= 5; i++) {
-      starRating += i <= rating ? '★' : '☆';
-    }
+  // getStarRating(rating: number): string {
+  //   let starRating = '';
+  //   for (let i = 1; i <= 5; i++) {
+  //     starRating += i <= rating ? '★' : '☆';
+  //   }
   
-    if (rating % 1 !== 0) {
-      // Check if the rating has a decimal value (e.g., 4.5)
-      const decimalPart = rating % 1;
-      starRating = starRating.slice(0, -1); // Remove the last star
-      if (decimalPart >= 0.25 && decimalPart <= 0.75) {
-        starRating += '⯪'; // Add a half-star symbol
-      } else if (decimalPart > 0.75) {
-        starRating += '★'; // Add a full star symbol
-      } else {
-        starRating += '☆'; // Add an empty star symbol
-        // const solidStarSymbol = '\u2605'; // ★
-      }
+  //   if (rating % 1 !== 0) {
+  //     // Check if the rating has a decimal value (e.g., 4.5)
+  //     const decimalPart = rating % 1;
+  //     starRating = starRating.slice(0, -1); // Remove the last star
+  //     if (decimalPart >= 0.25 && decimalPart <= 0.75) {
+  //       starRating += '⯪'; // Add a half-star symbol
+  //     } else if (decimalPart > 0.75) {
+  //       starRating += '★'; // Add a full star symbol
+  //     } else {
+  //       starRating += '☆'; // Add an empty star symbol
+  //       // const solidStarSymbol = '\u2605'; // ★
+  //     }
+  //   }
+
+  //   return starRating;
+  // }
+
+  getStarArray(rating: number): number[] {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 === 0.5;
+
+    const starArray = Array(fullStars).fill(1);
+    if (hasHalfStar) {
+      starArray.push(0.5);
     }
 
-    return starRating;
+    return starArray;
   }
-
+  
 
 
   selectRating(rating: number) {
